feef33717737a09828a24860e26bab6960ea7df8
Aaron Hurley <ahurley@pivotal.io>
2018-08-22 09:49:23 -0700

    set defaultTimeout to 30s [finishes #159965794]

--- istio_scaling_suite_test.go
+++ istio_scaling_suite_test.go
@@ -22,7 +22,7 @@ var (
 	cfg            config.Config
 	testPlan       config.TestPlan
 	testSetup      *workflowhelpers.ReproducibleTestSuiteSetup
-	defaultTimeout = 120 * time.Second
+	defaultTimeout = 30 * time.Second
 )
 
 var _ = BeforeSuite(func() {
